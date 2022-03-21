9f3131760bf0686f019690ef4802539c274f8083
DESKTOP-A8SPPMM\georg <georgedanicico@yahoo.com>
2022-03-22 00:39:21 +0200

    Lab1

---
+++ Assignment1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Assignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dBDataSet = new Assignment1.DBDataSet();
+            this.playersDataGridView = new System.Windows.Forms.DataGridView();
+            this.teamsDataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playersDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teamsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dBDataSet
+            // 
+            this.dBDataSet.DataSetName = "DBDataSet";
+            this.dBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // playersDataGridView
+            // 
+            this.playersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.playersDataGridView.Location = new System.Drawing.Point(24, 48);
+            this.playersDataGridView.Name = "playersDataGridView";
+            this.playersDataGridView.Size = new System.Drawing.Size(640, 220);
+            this.playersDataGridView.TabIndex = 1;
+            // 
+            // teamsDataGridView
+            // 
+            this.teamsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.teamsDataGridView.Location = new System.Drawing.Point(24, 323);
+            this.teamsDataGridView.Name = "teamsDataGridView";
+            this.teamsDataGridView.Size = new System.Drawing.Size(300, 220);
+            this.teamsDataGridView.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(774, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 71);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(774, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 71);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 624);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.teamsDataGridView);
+            this.Controls.Add(this.playersDataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playersDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teamsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DBDataSet dBDataSet;
+        private System.Windows.Forms.DataGridView playersDataGridView;
+        private System.Windows.Forms.DataGridView teamsDataGridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Assignment1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Assignment1
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection connection;
+        SqlDataAdapter daPlayer, daTeam;
+        DataSet ds;
+        SqlCommandBuilder cb;
+        BindingSource bsPlayer, bsTeam;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(@"Data Source = DESKTOP-A8SPPMM; Initial Catalog=DB_COURSE; Integrated Security = True");
+            ds = new DataSet();
+            daPlayer = new SqlDataAdapter("SELECT * FROM Players", connection);
+            daTeam = new SqlDataAdapter("SELECT * FROM Teams", connection);
+            cb = new SqlCommandBuilder(daPlayer);
+
+            daPlayer.Fill(ds, "Players");
+            daTeam.Fill(ds, "Teams");
+
+            DataRelation dr = new DataRelation("FK_Teams_Players", ds.Tables["Teams"].Columns["team_id"], ds.Tables["Players"].Columns["team_id"]);
+            ds.Relations.Add(dr);
+
+            bsPlayer = new BindingSource();
+            bsTeam = new BindingSource();
+
+            bsTeam.DataSource = ds;
+            bsTeam.DataMember = "Teams";
+
+            bsPlayer.DataSource = bsTeam;
+            bsPlayer.DataMember = "FK_Teams_Players";
+
+            playersDataGridView.DataSource = bsPlayer;
+            teamsDataGridView.DataSource = bsTeam;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            daPlayer.Update(ds, "Players");
+        }
+
+        private void playersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+/*            this.playersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dBDataSet);*/
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+/*            // TODO: This line of code loads data into the 'dBDataSet.Teams' table. You can move, or remove it, as needed.
+            this.teamsTableAdapter.Fill(this.dBDataSet.Teams);
+            // TODO: This line of code loads data into the 'dBDataSet.Players' table. You can move, or remove it, as needed.
+            this.playersTableAdapter.Fill(this.dBDataSet.Players);*/
+
+        }
+    }
+}
